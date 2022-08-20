91928e0b8e3736558883cb8737f166d38b04f88b
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-08-20 14:31:44 -0700

    get accel/gyro sensitivity compiling

--- Firmware/include/wallaby_imu.h
+++ Firmware/include/wallaby_imu.h
@@ -1,4 +1,14 @@
+#ifndef WALLABY_IMU_H
+#define WALLABY_IMU_H
+
 #include "stm32f4xx.h"
 
 void setupIMU();
 void readIMU();
+
+void setGyroSensitivity(uint8_t sensitivity);
+void setAccelSensitivity(uint8_t sensitivity);
+
+uint8_t getCachedGyroSensitivity();
+uint8_t getCachedAccelSensitivity();
+#endif /* WALLABY_IMU_H */
