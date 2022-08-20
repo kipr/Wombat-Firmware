8a77156cf511b942b0fbcdcff2337eb6183f55f4
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-08-20 11:03:34 -0700

    Revert "format main.c"
    This reverts commit 7ce73e1012bee807d1f6204939df7cb2ec8e3910.

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -18,10 +18,12 @@
 
 #include "wallaby.h"
 
+ 
+
 int main()
 {
-    int32_t bemf_vals[4] = {0, 0, 0, 0};
-    int32_t bemf_vals_filt[4] = {0, 0, 0, 0};
+    int32_t bemf_vals[4] = {0,0,0,0};
+    int32_t bemf_vals_filt[4] = {0,0,0,0};
 
     init();
 
@@ -32,22 +34,18 @@ int main()
     // set up pid structs
     pid_struct pid_structs[4];
 
-    {
-        uint8_t i;
-        for (i = 0; i < 4; ++i)
-        {
-            init_pid_struct(&(pid_structs[i]), i);
-        }
+    {        uint8_t i;
+        for (i = 0; i < 4; ++i) init_pid_struct(&(pid_structs[i]), i);
     }
 
     update_dig_pin_configs();
     config_adc_in_from_regs();
 
-    // debug_printf("starting\n");
+    //debug_printf("starting\n");
     int low_volt_alarmed = 0;
 
     setupIMU();
-
+    
     // Loop until button is pressed
     uint32_t count = 0;
     while (1)
@@ -55,17 +53,17 @@ int main()
         count += 1;
         {
             // only sample motor backemf 1/4 of the time
-            const uint8_t bemf_update_time = (count % 4 == 1);
+            const uint8_t bemf_update_time =  (count % 4 == 1);
             if (bemf_update_time)
             {
                 // idle breaking
-                MOT0_DIR1_PORT->BSRRH |= MOT0_DIR1;
+                MOT0_DIR1_PORT->BSRRH |= MOT0_DIR1; 
                 MOT0_DIR2_PORT->BSRRH |= MOT0_DIR2;
-                MOT1_DIR1_PORT->BSRRH |= MOT1_DIR1;
+                MOT1_DIR1_PORT->BSRRH |= MOT1_DIR1; 
                 MOT1_DIR2_PORT->BSRRH |= MOT1_DIR2;
-                MOT2_DIR1_PORT->BSRRH |= MOT2_DIR1;
+                MOT2_DIR1_PORT->BSRRH |= MOT2_DIR1; 
                 MOT2_DIR2_PORT->BSRRH |= MOT2_DIR2;
-                MOT3_DIR1_PORT->BSRRH |= MOT3_DIR1;
+                MOT3_DIR1_PORT->BSRRH |= MOT3_DIR1; 
                 MOT3_DIR2_PORT->BSRRH |= MOT3_DIR2;
             }
             // let the motor coast
@@ -74,11 +72,10 @@ int main()
             uint32_t before = usCount;
             update_dig_pins();
             int16_t batt = adc_update();
-
-            if (count % 2 == 0)
-            {
-                readIMU();
-            }
+            //readAccel();
+            //readMag();
+            //readGyro();   
+            if(count%2==0)readIMU();
 
             if (batt < 636) // about 5.75 volts
             {
@@ -120,7 +117,7 @@ int main()
             if (got_time_to_burn)
             {
                 // sleep the remainder of the coasting period before sampling bemf
-                delay_us(us_delay_needed - sensor_update_time);
+                delay_us(us_delay_needed-sensor_update_time);
             }
 
             if (bemf_update_time)
@@ -133,10 +130,11 @@ int main()
                 uint8_t channel;
                 for (channel = 0; channel < 4; ++channel)
                 {
-                    uint8_t shift = 2 * channel;
+                    uint8_t shift = 2*channel;
                     uint8_t motor_mode = (aTxBuffer[REG_RW_MOT_MODES] & (0b11 << shift)) >> shift;
                     motor_update(bemf_vals[channel], bemf_vals_filt[channel], &pid_structs[channel], channel, motor_mode);
                 }
+
             }
             else
             {
@@ -145,9 +143,9 @@ int main()
                 delay_us(222);
             }
 
-            // aTxBuffer[REG_RW_MOT_SRV_ALLSTOP] = 0;//FIXME: remove
+            //aTxBuffer[REG_RW_MOT_SRV_ALLSTOP] = 0;//FIXME: remove
         }
-    }
+    } 
 
     // set all motor pwms to 0
     aTxBuffer[REG_RW_MOT_0_PWM_H] = 0;
@@ -164,5 +162,5 @@ int main()
 
     spi2_dma_cleanup();
 
-    // debug_printf("done\n");
+    //debug_printf("done\n");
 }
