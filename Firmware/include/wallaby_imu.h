66d5060c897df224856fa3a188d1df7ba020c7bc
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-08-20 11:03:53 -0700

    Revert "configure scaling"
    This reverts commit 8417924224f7825d3016cee9506b57b71f1b7bd2.

--- Firmware/include/wallaby_imu.h
+++ Firmware/include/wallaby_imu.h
@@ -2,3 +2,11 @@
 
 void setupIMU();
 void readIMU();
+
+void setupAccelMag();
+void setupGyro();
+
+void readAccel();
+void readMag();
+void readGyro();
+
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p build && cd build
-cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=../CMake/GNU-ARM-Toolchain.cmake" ..
-cmake -- build ..
+
+cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=../CMake/GNU-ARM-Toolchain.cmake" ../
+cmake --build . -- -j 4
