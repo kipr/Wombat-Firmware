0123fc64abf257987e7e78b6b1e98ec42c8f6a28
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-09-06 08:30:49 -0700

    just have the sample rate change

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-mkdir -p build && cd build
-
-cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=../CMake/GNU-ARM-Toolchain.cmake" ../
-cmake --build . -- -j 4
+#/bin/bash
+mkdir build && cd build
+cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../CMake/GNU-ARM-Toolchain.cmake
+make -j4
